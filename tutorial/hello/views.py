7e4cd01661d746c14ea2090b69f83c444d5fecf9
uaciej <amacedmace@gmail.com>
2022-12-03 14:01:34 +0100

    added maciej

--- tutorial/hello/views.py
+++ tutorial/hello/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def maciej(request):
+    return HttpResponse("Hello, Maciej!")
