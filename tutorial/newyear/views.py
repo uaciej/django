3c3a267958fc998d3781b63968289ab8782b6d5e
uaciej <amacedmace@gmail.com>
2022-12-03 14:49:12 +0100

    added newyear

--- tutorial/newyear/views.py
+++ tutorial/newyear/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime as dt
 
 # Create your views here.
+def index(request):
+    now = dt.now()
+    return render(request, "newyear/index.html",{
+        "newyear": now.month == 1 and now.day == 1
+    })
